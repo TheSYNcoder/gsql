153ff0159b99a45cd22b0e2968e8190595c99f56
Shuvayan Ghosh Dastidar <papaigd@gmail.com>
2021-10-28 13:00:53 +0530

    fixed errors

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -1,42 +1,34 @@
-
-
 from gsql.frontend.shell.shell import GSQLShell
 import os
 import io
-import sys
-import pytest
-
-
-
 
 
 def test_shell_precmd_clear(monkeypatch, mocker):
 
     monkeypatch.setattr("sys.stdin", io.StringIO("CLEAR\nEXIT"))
-    mocker.patch.object(os, 'system')
+    mocker.patch.object(os, "system")
     instance = GSQLShell()
-    instance.cmdloop()             
-    os.system.assert_called_with('clear')
+    instance.cmdloop()
+    os.system.assert_called_with("clear")
+
 
 def test_shell_precmd_cls(monkeypatch, mocker):
 
     monkeypatch.setattr("sys.stdin", io.StringIO("CLEAR\nEXIT"))
-    mocker.patch.object(os, 'system')
-    mocker.patch.object(os, 'name')
-    os.name.return_value = 'windows'
+    mocker.patch.object(os, "system")
+    mocker.patch.object(os, "name")
+    os.name.return_value = "windows"
     instance = GSQLShell()
-    instance.cmdloop()             
-    os.system.assert_called_with('cls')
+    instance.cmdloop()
+    os.system.assert_called_with("cls")
 
 
 def test_shell_precmd_connect_correctly(monkeypatch, mocker):
 
-    monkeypatch.setattr("sys.stdin", io.StringIO("CONNECT 1WooAUEpz7ECEK2M7YIS3WzNK2c\nEXIT"))
+    monkeypatch.setattr(
+        "sys.stdin", io.StringIO("CONNECT 1WooAUEpz7ECEK2M7YIS3WzNK2c\nEXIT")
+    )
     instance = GSQLShell()
-    instance.cmdloop()  
-    sheet_name = 'Fun Sheet'
+    instance.cmdloop()
+    sheet_name = "Fun Sheet"
     assert instance.prompt == f'GSQL ({sheet_name.replace(" ", "")[:10]}) > '
-    
-    
-
-
