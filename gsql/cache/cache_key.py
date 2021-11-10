2047c35e7bcd2108bf036718bb22b108e72e0cc5
skm2000 <shubhamskm2000@gmail.com>
2021-11-10 14:58:04 +0530

    refractored

--- gsql/cache/cache_key.py
+++ gsql/cache/cache_key.py
@@ -1,74 +1,64 @@
-from functools import lru_cache
 from key_pair import Keypair
-from gsql.cache.cache_handler import (
-    CacheDict,
-    check_json_file_exists,
-    count_lines_in_json,
-    create_json_file,
-    keys_exists,
-    read_from_json,
-    remove_key_from_json,
-    write_to_json,
-)
+from gsql.cache.cache_handler import CacheDict
 from gsql.cache.constant import MAXIMUM_SIZE_OF_JSON_FILE
+from gsql.logging import logger
 
-
-"""
-    Cachekey contains the type of query,
-    the query itself, database id and the sheet id.
-"""
+CACHE = CacheDict.Dict
 
 
 class Cachekey:
+    """
+    Cachekey contains the type of query,
+    the query itself, database id and the sheet id.
+    """
+
     def __init__(self, query_type, query, db_id, sheet_id) -> None:
         self.query_type = query_type
         self.query = query
         self.db_id = db_id
         self.sheet_id = sheet_id
 
-    @lru_cache(maxsize=10000)
     def validate(self):
         """
-        Check if the query is executed before
+        Check if the query is executed before based on the type of query
         """
         keypair = Keypair(self.db_id, self.sheet_id).generate_hash()
         if self.query_type == "D" or self.query_type == "U":
-            if keys_exists(CacheDict, keypair) is True:
-                del CacheDict[keypair]
+            if CacheDict.keys_exists(CACHE, keypair) is True:
+                logger.debug("Query found in cache dict..")
+                del CACHE[keypair]
+                logger.info("Deleted suceesfully from cache dict..")
             else:
-                print("Query not in cache...")
-            remove_key_from_json(keypair)
-            self.clear_cache()
+                logger.debug("Query not found in cache dict...")
+            CacheDict.remove_key_from_json(keypair)
             return None
+
         querykey = Keypair(self.query, keypair).generate_hash()
-        if keys_exists(CacheDict, keypair, querykey):
-            print("Key found in the cache.....")
-            return CacheDict[keypair][querykey]
+        if CacheDict.keys_exists(CACHE, keypair, querykey):
+            logger.debug("Key found in the cache dict..")
+            return CACHE[keypair][querykey]
         else:
-            file_exists = check_json_file_exists()
+            file_exists = CacheDict.check_json_file_exists()
             if not file_exists:
-                create_json_file()
-            read_data = read_from_json(keypair, querykey)
-            if keys_exists(CacheDict, keypair) is False:
-                CacheDict[keypair] = {}
+                CacheDict.create_json_file()
+
+            read_data = CacheDict.read_from_json(keypair, querykey)
+            if CacheDict.keys_exists(CACHE, keypair) is False:
+                CACHE[keypair] = {}
+
             if read_data is None:
                 # call to database
-                print("Key not found in the log file.....")
+                logger.debug("Key not found in the log file.....")
                 value = "database call value"
-                CacheDict[keypair][querykey] = value
-                write_to_json(value, keypair, querykey)
+                CACHE[keypair][querykey] = value
+                CacheDict.write_to_json(value, keypair, querykey)
+
             else:
-                print("Key found in log file, please wait.....")
-                CacheDict[keypair][querykey] = read_data
+                logger.debug("Key found in log file, please wait.....")
+                CACHE[keypair][querykey] = read_data
                 return read_data
 
-    def clear_cache(self):
-        """
-        clear the lru cache once completed
-        """
-        self.validate.cache_clear()
-
-    def print_current_and_maxm_contents(filename):
-        if check_json_file_exists():
-            print("Number of lines in json", count_lines_in_json())
+    def current_and_maxm_contents(filename):
+        if CacheDict.check_json_file_exists():
+            print("Number of lines in json", CacheDict.count_lines_in_json())
             print("Maximum lines in json ", MAXIMUM_SIZE_OF_JSON_FILE)
