e4c5226bc907536e9780e1cbaf73838019514109
skm2000 <shubhamskm2000@gmail.com>
2021-11-10 14:55:45 +0530

    oops method followed

--- gsql/cache/cache_handler.py
+++ gsql/cache/cache_handler.py
@@ -1,107 +1,77 @@
 import json
 import os
-from json.decoder import JSONDecodeError
+from gsql.logging import logger
 
 
-CacheDict = {}
+class CacheDict:
 
+    Dict = {}
 
-def keys_exists(element, *keys):
-    """
-    Check if *keys (nested) exists in `element` (dict).
-    """
-    if not isinstance(element, dict):
-        raise AttributeError("keys_exists() expects dict as first argument.")
-    if len(keys) == 0:
-        raise AttributeError(
-            "keys_exists() expects at least three arguments, one given."
-        )
+    def keys_exists(keystore, *keys):
+        """
+        Check if *keys (nested) exists in `keystore` (dict).
+        """
+        _keystore = keystore
+        for key in keys:
+            try:
+                _keystore = _keystore[key]
+            except KeyError:
+                return False
+        return True
 
-    _element = element
-    for key in keys:
-        try:
-            _element = _element[key]
-        except KeyError:
-            return False
-    return True
-
-
-def count_lines_in_json(filename="log_data.json"):
-    with open(filename, "r") as file:
-        try:
+    def count_lines_in_json(filename="log_data.json"):
+        """
+        Count number of lines in a json file.
+        """
+        with open(filename, "r") as file:
             file_data = json.load(file)
             return len(file_data)
-        except JSONDecodeError as error:
-            print(error)
 
+    def check_json_file_exists(filename="log_data.json") -> bool:
+        if os.path.exists(filename):
+            return True
+        else:
+            return False
 
-def check_json_file_exists(filename="log_data.json") -> bool:
-    if os.path.exists(filename):
-        return True
-    else:
-        return False
+    def create_json_file(filename="log_data.json"):
+        with open(filename, "w") as file:
+            logger.info("File created successfully")
+            json.dump({}, file)
+            file.close()
 
-
-def create_json_file(filename="log_data.json"):
-    with open(filename, "w") as file:
-        print("File created successfully")
-        json.dump({}, file)
-        file.close()
-
-
-def remove_key_from_json(keypair, filename="log_data.json"):
-    if check_json_file_exists() is True:
-        with open(filename, "r") as file:
-            try:
+    def remove_key_from_json(keypair, filename="log_data.json"):
+        """
+        Remove key from json if exits
+        """
+        if CacheDict.check_json_file_exists() is True:
+            with open(filename, "r") as file:
                 file_data = json.load(file)
-                if keys_exists(file_data, keypair) is True:
+                if CacheDict.keys_exists(file_data, keypair) is True:
                     del file_data[keypair]
-                    print("key removed successfully from file")
-            except JSONDecodeError as error:
-                print(error)
-        with open(filename, "w") as modified_file:
-            try:
+                    logger.info("key removed successfully from file")
+            with open(filename, "w") as modified_file:
                 json.dump(file_data, modified_file, indent=2)
-                print("file modified successfully")
-            except JSONDecodeError as error:
-                print(error)
-    else:
-        print("file does not exists")
+                logger.debug("file modified successfully")
+        else:
+            logger.info("file does not exists")
 
-
-def read_from_json(keypair, querypair, filename="log_data.json"):
-    if check_json_file_exists(filename) is True:
+    def read_from_json(keypair, querypair, filename="log_data.json"):
         with open(filename, "r") as file:
-            try:
-                file_data = json.load(file)
-                if keys_exists(file_data, keypair, querypair):
-                    print("Data present in json file")
-                    return file_data[keypair][querypair]
-            except JSONDecodeError as error:
-                print(error)
-    else:
-        return
-
-
-def append_to_json(data, filename="log_data.json", *keys):
-    with open(filename, "r+") as file:
-        try:
             file_data = json.load(file)
-            if keys_exists(file_data, keys[0]) is False:
+            if CacheDict.keys_exists(file_data, keypair, querypair):
+                logger.info("Data present in log file")
+                return file_data[keypair][querypair]
+
+    def append_to_json(data, filename="log_data.json", *keys):
+        with open(filename, "r+") as file:
+            file_data = json.load(file)
+            if CacheDict.keys_exists(file_data, keys[0]) is False:
                 file_data[keys[0]] = {}
             file_data[keys[0]][keys[1]] = data
             file.seek(0)
             json.dump(file_data, file, indent=2)
-            print("Data appended successfully...............")
-        except JSONDecodeError as error:
-            print(error)
+            logger.info("Data appended successfully...............")
 
-
-def write_to_json(data, *keys):
-    filename = "log_data.json"
-    if len(keys) == 0:
-        raise AttributeError(
-            "write_to_json() expects at least one argument, none given."
-        )
-    else:
-        append_to_json(data, filename, *keys)
+    def write_to_json(data, *keys):
+        filename = "log_data.json"
+        CacheDict.append_to_json(data, filename, *keys)
